f0efc29d28c8b8c55d1f9e20adb9063cefceabcb
hannah barker <hannah.m.barker@gmail.com>
2019-03-17 23:06:36 -0400

    resolved issues with jquery

--- main.js
+++ main.js
@@ -1,38 +1,36 @@
 $(()=>{
     $(".table").on("click", (event) => {
         console.log($(event.target).attr("id"));
-        $("#form").removeClass("hide");
-        $("#form").addClass("show");
+        //$("#form").removeClass("hide");
+        //$("#form").addClass("show");
+        $('#form').attr('class', 'show');
         
-
-
-
-
-        let tableNumber = (event.target.firstChild.data);
+        let tableNumber =  $(event.target).text();
         $("#table-number").text(tableNumber);
+        //$(document.getElementById('table-number')).text(tableNumber);
 
 
-
-        let tableNumberId = `#table-${tableNumber}`;
+        let clearForm = () => {
+            $("#form input").val("");
+        }
 
         $("#save").on("click", () => {
-            $(tableNumberId).removeClass("available");
-            $(tableNumberId).addClass("reserved");
-            $(tableNumberId).prop("disabled", true);
-            $(`#form`).removeClass("show");
-            $(`#form`).addClass("hide");
-            console.log($("#name").val(""));
-            console.log($("#number").val(""));
-            console.log($("#group").val(""));
+            $(event.target).removeClass("available");
+            $(event.target).addClass("reserved");
+            $(event.target).prop("disabled", true);
+            //$("#form").removeClass("show");
+            //$("#form").addClass("hide");
+            $('#form').attr('class', 'hide');
+            console.log($("#name").val());
+            console.log($("#number").val());
+            console.log($("#group").val());
+            clearForm();
         });
 
         $("#exit-button").on("click", () => {
-            tableNumberId = null;
             $("#form").removeClass("show");
             $("#form").addClass("hide");
-            $("#name").val("");
-            $("#number").val("");
-            $("#group").val("");
+            clearForm();
         });
     });
     
